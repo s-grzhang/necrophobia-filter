edb4ca1f3fae367a810f7d2e3e2344bd896c2afe
Grace Zhang <birdsrforever@outlook.com>
2022-10-08 20:18:00 -0700

    bing-image-downloader works google-images-downloader doesn't work

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 enterQuery = input("What do you want to search?")
 print(f"Searching for {enterQuery}")
 
+# from googlesearch import search
+# results = search(enterQuery, num_results=100)
+# for result in results:
+#     print(result)
 
-from googlesearch import search
-results = search(enterQuery, num_results=100)
-for result in results:
-    print(result)
+# from bing-image-downloader import google_images_download   #importing the library
+#
+# response = google_images_download.googleimagesdownload()   #class instantiation
+#
+# arguments = {"keywords":"Polar bears,baloons,Beaches","limit":20,"print_urls":True}   #creating list of arguments
+# paths = response.download(arguments)   #passing the arguments to the function
+# print(paths)   #printing absolute paths of the downloaded images
+
+from bing_image_downloader import downloader
+
+downloader.download(enterQuery, limit=100, output_dir='dataset', adult_filter_off=True, force_replace=False, timeout=60)
